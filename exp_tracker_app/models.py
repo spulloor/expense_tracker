617ec3a88f328290b478c83b1bc817f6cc63271f
Suraj Pulloor <pulloor.suraj.oss@gmail.com>
2024-04-26 13:08:29 +0000

    Added model 'Item'

--- exp_tracker_app/models.py
+++ exp_tracker_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+import uuid
+
+class Item(models.db):
+    name = models.CharField(required=True, max_length=30)
+    price = models.DecimalField(required=True, max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(required=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    location = models.CharField(max_length=30)
+    
