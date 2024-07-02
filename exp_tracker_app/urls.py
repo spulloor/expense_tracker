e366c374a439d533c96d72ba60c92ca113aa563a
Suraj Pulloor <suraj.pulloor.oss@gmail.com>
2024-07-03 00:08:53 +0530

    Added a couple of views and their corresponding urlconfs

--- exp_tracker_app/urls.py
+++ exp_tracker_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import register, login_view
+from .views import *
 
 
 app_name = 'exp_tracker_app'
 
 urlpatterns = [
+    path('', dashboard, name='dashboard'),
     path('register', register, name='register'),
     path('login', login_view, name='login'),
+    path('create_expense', create_expense, name='create_expense'),
+    path('profile', profile, name='profile'),
+    path('logout_from_app', logout_from_app, name='logout_from_app'),
 ]
--- exp_tracker_app/views.py
+++ exp_tracker_app/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import RegistrationForm, LoginForm
-from django.http import HttpResponse
+from django.contrib.auth import login, logout
+from .forms import RegistrationForm, LoginForm, ItemForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def register(request):
+
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('exp_tracker_app:dashboard'))
+
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             # Optionally, send a welcome email or redirect to a success page
-            return redirect('expense_tracker_app:login')  # Redirect to login page after registration
+            return redirect('exp_tracker_app:login')  # Redirect to login page after registration
     else:
         form = RegistrationForm()
     return render(request, 'exp_tracker_app/register.html', {'form': form})
@@ -18,16 +23,56 @@ def register(request):
 def login_view(request):
 
     if request.user.is_authenticated:
-        return HttpResponse("The user is authenticated")
+        return HttpResponseRedirect(reverse('exp_tracker_app:dashboard'))
     
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             user = form.cleaned_data['user']  # Access authenticated user from form
             login(request, user)
-            return redirect('expense_tracker_app:dashboard')  # Redirect to dashboard after successful login
+            return redirect('exp_tracker_app:dashboard')  # Redirect to dashboard after successful login
     else:
         form = LoginForm()
 
     context = {'form': form}
     return render(request, 'exp_tracker_app/login.html', context)
+
+
+def dashboard(request):
+
+    if request.user.is_authenticated:
+        return render(request, 'exp_tracker_app/dashboard.html')
+    else:
+        return HttpResponseRedirect(reverse('exp_tracker_app:login'))
+
+
+def create_expense(request):
+
+    if request.user.is_authenticated:
+
+        if request.method == 'POST':
+            form = ItemForm(request.POST)
+            
+            if form.is_valid():
+                expense = form.save(commit=False)
+                expense.created_by = request.user
+                form.save()
+
+                return redirect('expense_tracker:dashboard')
+        else:
+            form = ItemForm()
+
+        return render(request, 'exp_tracker_app/create_expense.html', {'form': form})
+    
+
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'exp_tracker_app/profile.html')
+    else:
+        return HttpResponseRedirect(reverse('exp_tracker_app:login'))
+    
+def logout_from_app(request):
+    if request.user.is_authenticated:
+        logout(request)
+        
+    return HttpResponseRedirect(reverse('exp_tracker_app:login'))
